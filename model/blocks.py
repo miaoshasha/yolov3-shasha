515e5db2bbd50e4f26013cd11f01e4c41f68e353
Yisha Sun <yishasun@yishas-mbp.gateway.sonic.net>
2020-03-05 22:23:14 -0800

    first commit, backbone darknet53

---
+++ model/blocks.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from model.utils import compose
+
+class Conv2D_BN_LeakyRelu(tf.keras.layers.Layer):
+    def __init__(self, num_filter, kernel_size, down_sample=False):
+        super(Conv2D_BN_LeakyRelu, self).__init__(name='')
+        if down_sample:
+            stride = (2, 2)
+            padding = 'valid'
+        else:
+            stride = (1, 1)
+            padding = 'same'
+        kernel_size = (kernel_size, kernel_size)
+        self.conv = tf.keras.layers.Conv2D(num_filter, kernel_size, stride, padding)
+        self.bn = tf.keras.layers.BatchNormalization()
+        self.leaky = tf.keras.layers.LeakyReLU(alpha=0.1)
+
+    def call(self, input_tensor, training=False):
+        x = self.conv(input_tensor)
+        x = self.bn(x)
+        x = self.leaky(x)
+        return x
+
+def getResnetBlock(inputs, num_filter, num_repeat, training=False):
+    x = Conv2D_BN_LeakyRelu(num_filter, 3, down_sample=True)(inputs, training=training)
+    for i in range(num_repeat):
+        x = Conv2D_BN_LeakyRelu(num_filter//2, 1, down_sample=False)(x, training=training)
+        x = Conv2D_BN_LeakyRelu(num_filter, 3, down_sample=False)(x, training=training)
+        x = tf.keras.layers.Add()([x,inputs])
+    return x
+
+def getDarkNet53(inputs, training=False):
+    x = Conv2D_BN_LeakyRelu(32, 3, down_sample=True)(inputs)
+    x = getResnetBlock(x, 64, 1, training=training)
+    x = getResnetBlock(x, 128, 2, training=training)
+    x = getResnetBlock(x, 256, 8, training=training)
+    x = getResnetBlock(x, 512, 8, training=training)
+    x = getResnetBlock(x, 1024, 4, training=training)
+    return x
---
+++ model/model.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+
+
+class ResnetIdentityBlock(tf.keras.layers.Layer):
+  def __init__(self, kernel_size, filters):
+    super(ResnetIdentityBlock, self).__init__(name='')
+    filters1, filters2, filters3 = filters
+
+    self.conv2a = tf.keras.layers.Conv2D(filters1, (1, 1))
+    self.bn2a = tf.keras.layers.BatchNormalization()
+
+    self.conv2b = tf.keras.layers.Conv2D(filters2, kernel_size, padding='same')
+    self.bn2b = tf.keras.layers.BatchNormalization()
+
+    self.conv2c = tf.keras.layers.Conv2D(filters3, (1, 1))
+    self.bn2c = tf.keras.layers.BatchNormalization()
+
+  def call(self, input_tensor, training=False):
+    x = self.conv2a(input_tensor)
+    x = self.bn2a(x, training=training)
+    x = tf.nn.relu(x)
+
+    x = self.conv2b(x)
+    x = self.bn2b(x, training=training)
+    x = tf.nn.relu(x)
+
+    x = self.conv2c(x)
+    x = self.bn2c(x, training=training)
+
+    x += input_tensor
+    return tf.nn.relu(x)
+
+
+block = ResnetIdentityBlock(1, [1, 2, 3])
